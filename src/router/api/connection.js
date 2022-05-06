b02782bf29e4ab86f03341ab51111f5958b4cde6
anonymous <tom.ami@etu.univ-amu.com>
2022-05-06 15:59:58 +0200

    email validator login

--- src/router/api/connection.js
+++ src/router/api/connection.js
@@ -1,5 +1,13 @@
-import {signup_service, is_valid_req_signup_service} from "../../services/signup.js";
+import bodyParser from 'body-parser'
+
+import {signup_service, is_valid_req_signup_service, is_amu_email} from "../../services/signup.js";
+
+
 
 export const connection = (router)=>{
-    router.post('/api/signup', is_valid_req_signup_service, signup_service);
+    router.post('/api/signup',
+        bodyParser.urlencoded({ extended: true }),
+        is_valid_req_signup_service,
+        is_amu_email,
+        signup_service);
 }
--- src/services/signup.js
+++ src/services/signup.js
@@ -1,10 +1,35 @@
 const signup_service= (req, res)=>{
 
-    req
+    let mail = req.body.mail
+
+
+
+
     res.send("salut");
 };
 // mail
 // mot de passe
+
+/////////////////////////////////////
+////////////email validator /////////
+
+const amu_email_regex = '[a-zA-Z]+\.[a-zA-Z]+@[a-zA-Z]+\.univ-amu\.fr';
+
+const is_amu_email = (req, res, next)=>{
+    if(validate_amu_email(req.body.email)) next();
+    else{
+        res.statusMessage = "Invalid email";
+        res.status(406).end();
+    }
+};
+
+const validate_amu_email = (email)=>{
+    return email.match(
+        amu_email_regex
+    )
+};
+
+/////////////////////////////////////
 
 const is_valid_req_signup_service = (req, res, next)=>{
     if (req.body.email && req.body.password) next();
@@ -12,7 +37,9 @@ const is_valid_req_signup_service = (req, res, next)=>{
         res.statusMessage = "Invalid request";
         res.status(406).end();
     }
-}
+};
+
+
 
 
 
@@ -20,5 +47,6 @@ const is_valid_req_signup_service = (req, res, next)=>{
 
 export {
     signup_service,
-    is_valid_req_signup_service
+    is_valid_req_signup_service,
+    is_amu_email
 };
