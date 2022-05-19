6307bb9cce432b51cbbeaf420015f55cbed895b2
anonymous <tom.ami@etu.univ-amu.com>
2022-05-19 12:18:39 +0200

    now new user are produceur by default

--- src/models/signup.js
+++ src/models/signup.js
@@ -1,6 +1,7 @@
 import {pool} from "../config/database/database-config.js";
 
 const signup_query = "INSERT into user (user_email, password, first_name, last_name, key_verification) VALUES (?, ?, ?, ?, ?)";
+const insert_producer = "INSERT INTO producer (user_id) VALUES (?)";
 
 const signup_db =  async (data)=>{
     let insert_data = [data.mail, data.password, data.first_name, data.last_name, data.key];
@@ -8,6 +9,8 @@ const signup_db =  async (data)=>{
         const conn = await pool.getConnection();
         try {
             let result = await conn.query(signup_query, insert_data);
+            let lastID = Number(result.insertId);
+            await conn.query(insert_producer, [lastID]);
             await conn.release();
             resolve();
         }catch (err){
