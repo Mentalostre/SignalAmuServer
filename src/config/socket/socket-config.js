bffd4ca30bf61667075092648d22b0730ca6f313
anonymous <tom.ami@etu.univ-amu.com>
2022-05-19 19:52:23 +0200

    implement io socket connection and ping on new report

--- src/config/socket/socket-config.js
+++ src/config/socket/socket-config.js
@@ -7,8 +7,8 @@ export const io_config = (app)=>{
     const http_server = http.createServer(app);
     io = new Server(http_server);
     http_server.listen(process.env.IO_PORT || 3001);
-}
+};
 
 export const emit_new_report = ()=>{
     io.emit('report');
-}
+};
--- src/models/report.js
+++ src/models/report.js
@@ -2,7 +2,7 @@ import {pool} from "../config/database/database-config.js";
 
 const insert_post_query = "INSERT INTO report (report_desc, report_level, report_date, location_lat, location_long, tag_id, producer_id) VALUES (?,?,NOW(),?,?,?,(SELECT id FROM producer WHERE user_id = (SELECT id FROM user WHERE user_email=?)))";
 
-const get_query = "SELECT t1.id, t1.report_desc description, t1.report_level level, t1.report_date date, t1.location_lat, t1.location_long, status, t2.tag_name, t3.first_name, t3.last_name, t3.user_email FROM report t1 INNER JOIN tag t2 ON t1.tag_id = t2.id INNER JOIN user t3 ON (SELECT user_id FROM producer WHERE id = t1.producer_id  ) = t3.id WHERE t1.status = 0;\n";
+const get_query = "SELECT t1.id, t1.report_desc description, t1.report_level level, t1.report_date date, t1.location_lat, t1.location_long, status, t2.tag_name, t3.first_name, t3.last_name, t3.user_email FROM report t1 INNER JOIN tag t2 ON t1.tag_id = t2.id INNER JOIN user t3 ON (SELECT user_id FROM producer WHERE id = t1.producer_id  ) = t3.id WHERE t1.status = 0;";
 
 const report_post_db = (data)=>{
     return new Promise(async (resolve, reject)=>{
