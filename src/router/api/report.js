85117b1e60e29ec168a42783bc8039becb77e2f6
anonymous <tom.ami@etu.univ-amu.com>
2022-05-31 16:30:54 +0200

    deployed db

--- src/router/api/report.js
+++ src/router/api/report.js
@@ -13,7 +13,7 @@ import {
 } from "../../services/report.js";
 
 export const report = (router)=>{
-    router.post('/api/report', bodyParser.urlencoded({extended: true}), is_registered, is_valid_post_report ,report_service);
+    router.post('/api/report', bodyParser.urlencoded({extended: true}), is_valid_post_report ,report_service);
 
     router.get('/api/report', is_registered, report_get_service); // return all reports
 
