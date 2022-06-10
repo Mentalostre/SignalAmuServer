77434b8e8664a69242ec6c7327284223a97ac9ea
anonymous <tom.ami@etu.univ-amu.com>
2022-06-10 11:30:59 +0200

    response

--- src/models/admin.js
+++ src/models/admin.js
@@ -1,14 +1,14 @@
 import {pool} from "../config/database/database-config.js";
 
 const post_query = 'INSERT INTO consumer (user_id) VALUES (?)';
-const post_query_remove = 'DELETE FROM consumer WHERE id = ?'
+const post_query_remove = 'DELETE FROM consumer WHERE user_id = ?'
 
 
 const post_consumer_db = (id)=>{
     return new Promise(async (resolve, reject)=>{
         const conn = await pool.getConnection();
         try {
-            let result = conn.query(post_query,[id])
+            let result = await conn.query(post_query,[id])
             await conn.release();
             resolve(result.insertId)
         }catch (err){
@@ -21,7 +21,7 @@ const post_consumer_remove_db = (id)=>{
     return new Promise(async (resolve, reject)=>{
         const conn = await pool.getConnection();
         try {
-            conn.query(post_query_remove,[id])
+            let result = await conn.query(post_query_remove,[id])
             await conn.release();
             resolve()
         }catch (err){
